d96f159066a2ac5919cff97e0f19c4cfa7cfdca3
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-15 00:40:05 +0300

    Update ams.js

--- ams.js
+++ ams.js
@@ -96,39 +96,7 @@ client.on('message', async msg => {
       .setImage(data)
     msg.channel.send({embed})
   } 
-  
-  if(['status', 'статус'].includes(command) && ['321268938728144906', '341988428457705482']") {
-        let new_args = args;
-        if (new_args[0].toLowerCase() === 'играет' && new_args[1].toLowerCase() === 'в') {
-            new_args[0] = 'играет в';
-            new_args.splice(1, 1);
-        }
-        let type = new_args.shift();
-        let real_type;
-        if (['играет в', 'играет', 'play', 'playing', '0'].includes(type.toLowerCase()))
-            real_type = 0;
-        else if (['слушает', 'hear', 'hearing', '2'].includes(type.toLowerCase()))
-            real_type = 2;
-        else if (['смотрит', 'watch', 'watching', '3'].includes(type.toLowerCase()))
-            real_type = 3;
-        else return message.channel.send(`Ебобо? -> \`${type.replace(/` /g, "\'")}\` не существует`);
-        const status = new_args.join(" ");
-        client.user.setPresence({ game: { name: status, type: real_type } }).catch();
-        let status_word;
-        if (real_type === 0)
-            status_word = 'Играет в';
-        else if (real_type === 2)
-            status_word = 'Слушает';
-        else if (real_type === 3)
-            status_word = 'Смотрит';
-
-        const embed = new Discord.RichEmbed()
-            .setTitle('Статус изменен на:')
-            .setDescription(`${status_word} **${status.replace(/` /g, "\\\'")}**`)
-            .setFooter('Presence');
-        message.channel.send({embed});
-        message.delete();
-                    
+                     
 });
 
 client.login(process.env.TOKEN)
