1ae357b4f19a30d22f08cf8c1b484a67c04e11c7
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-15 00:45:08 +0300

    Update ams.js

--- ams.js
+++ ams.js
@@ -7,7 +7,7 @@ var owners = ['321268938728144906', '341988428457705482']
 
 client.on('ready', () => {
   console.log(client.user.tag);
-  client.user.setActivity('Слава Кабанов', {type: 'WATCHING'})
+  client.user.setActivity('#help', {type: 'STREAMING'})
 });
 
 client.on('guildMemberAdd', member => {
