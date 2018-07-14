041f4fa775e9cce9e502374b10da6de295fa4f12
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-14 15:50:48 +0300

    Update ams.js

--- ams.js
+++ ams.js
@@ -35,7 +35,6 @@ client.on('message', async msg => {
   }
 
   if (['eval', 'евал'].includes(cmd) && owners.includes(msg.author.id)) {
-    return message.reply("Прости, но ты не можешь использовать это!")
     var code = args.join(' ');
     try {
       let evaled = eval(code);
