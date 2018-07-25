f0aa1d12fbe4afd2ec2d7e10f7db13cfb321d315
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-25 23:45:30 +0300

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,10 +1,12 @@
+var getImageColors = require('get-image-colors');
+
 exports.info = {description: 'Спиздить чей-то аватар', required: 'SEND_MESSAGES'}
 exports.run = (client, msg, args) => {
                 var mentions1 = message.mentions
 		const embed = new Discord.RichEmbed()
 		.setTitle('Аватар пользователя:')
-            .setColor('#ffffff')
-		 .setImage(message.mentions.users.first().avatarURL)
+                .setColor('#ffffff')
+	 .setImage(message.mentions.users.first().avatarURL)
 		 message.channel.send({embed})
 	}
 }
