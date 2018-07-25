0405c7fb187eb95a8006619ea6f861b6362981a9
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-25 11:50:12 +0300

    хм ..

---
+++ cmds/eval.js
@@ -0,0 +1,22 @@
+  var code = args.join(' ');
+    try {
+      let evaled = eval(code);
+      if (!code) {
+        return msg.channel.send('Для выполнения команды eval необходим код');
+      }
+      if (typeof evaled !== 'string')
+        evaled = require('util').inspect(evaled)
+        var embed = new Discord.RichEmbed()
+          .setTitle(`Эвал успешно выполнен`)
+          .setColor('0x4f351')
+          .setDescription(`📥 Input: \n \`\`\`${code}\`\`\` \n 📤 Output: \n  \`\`\`${(evaled)}\`\`\``)
+        msg.channel.send({embed});
+    } catch (err) {
+      var embed = new Discord.RichEmbed()
+        .setTitle('Ошибка выполнения кода')
+        .setColor('0xff0202')
+        .setDescription(`📥 Input: \n \`\`\`${code}\`\`\`\n 📤 Output:\n  \`\`\`${(err)}\`\`\``)
+      msg.channel.send({embed});
+      msg.delete();
+   }
+ }
