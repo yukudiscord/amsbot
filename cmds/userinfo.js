7872adfd24ef10aae5c599d911cd024bb065ee6a
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-25 23:28:38 +0300

    Delete userinfo.js

--- cmds/userinfo.js
+++
@@ -1,8 +0,0 @@
-exports.info = {description: 'Информация о пользователе', required: 'SEND_MESSAGES'}
-exports.run = async (client, msg, args) => {
-
-var embed = new Dicord.RichEmbed()
-  .setTitle("Информация о пользователе")
-  .setDescription('**Ник**\n${msg.author.nickname}\n**Дискриминатор**\n${msg.author.discriminator}\n**Пользователь бот?**\n${msg.author.bot}\n**Айди**\n${msg.author.id}')
-  .setThumbnail("https://cdn.discordapp.com/attachments/471411944880996353/471774813921476609/icons8-info-squared-80.png")
-  msg.channel.send({embed})
