17594fbb663e3ed4f0e1a530ca98a67f6de26ea8
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-26 01:14:27 +0300

    Update vote.js

--- cmds/vote.js
+++ cmds/vote.js
@@ -7,7 +7,8 @@ var embed = new Discord.RichEmbed()
 .setDescription(vote)
 .setColor('RANDOM')
 .setFooter('Poll');
-msg.guild.channels.find('name', 'votes').send({embed})
+client.fetchwebhook('471794934354018306, 'process.env.WEBHOOK_ID').then(webhook -> {
+      webhook.send('', {username: nick, avatarURL: msg.author.avatarURL, embeds:  [embed]}).catch(console.error);
 await msg.react('${client.emojis.get(client.yes)}')
 await msg.react(client.emojis.get(client.no))
 }
