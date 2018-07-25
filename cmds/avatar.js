b7084a99dcda4ca3771a616ad7318a68dcd734b2
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-25 23:41:37 +0300

    Create avatar.js

---
+++ cmds/avatar.js
@@ -0,0 +1,8 @@
+		var mentions1 = message.mentions
+		const embed = new Discord.RichEmbed()
+		.setTitle('Аватар пользователя:')
+            .setColor('RANDOM')
+		 .setImage(message.mentions.users.first().avatarURL)
+		 message.channel.send({embed})
+	}
+}
