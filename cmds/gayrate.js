555f1be06db19f7d07b75cbc940223124107d6f0
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-25 23:17:55 +0300

    Update gayrate.js

--- cmds/gayrate.js
+++ cmds/gayrate.js
@@ -6,13 +6,13 @@ function reverse(x) {
   return parseInt(res)
 }
 
-exports.info = {description: 'Насколько юер гей? :thinking:', required: 'SEND_MESSAGES'}
+exports.info = {description: 'Насколько юзер гей? :thinking:', required: 'SEND_MESSAGES'}
 exports.run = async (client, msg, args) => {
   var u = msg.mentions.users.first()
   if(!u) u = msg.author
   var rated = reverse(u.id)%100
   var embed = new client.RichEmbed()
-    .setTitle('Измеритель века 2000')
+    .setTitle('Измеритель Гейства 3000')
     .setDescription(`<@${u.id}> на ${rated}% гей ${client.emojis.get(client.gaypride)}`)
   msg.channel.send({embed})
 }
