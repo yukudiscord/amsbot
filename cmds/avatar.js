51b886555d7bacdfa2086114de128a757998830b
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-25 12:12:16 +0300

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,1 +1,15 @@
-const getImageColors = require('get-image-colors')
+var getImageColors = require('get-image-colors')
+exports.info = {description: 'Украсть аватарку', required: 'MANAGE_MESSAGES'}
+exports.run = async (client, msg, args) => {
+          message.delete();
+        let user = message.mentions.users.first();
+        if (!user) user = message.author;
+        getImageColors(user.avatarURL).then(color => {
+            let c = color.map(col => col.hex());
+        let embed = new Discord.RichEmbed()
+            .setDescription(`Аватар ${user}`)
+            .setColor("#ffffff")
+            .setImage(user.avatarURL);
+        message.channel.send({embed});
+    } 
+  }
