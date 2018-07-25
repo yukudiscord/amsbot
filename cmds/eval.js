2528f6e5db53bd5c7f4ead6e3cdd412b491ac203
DanilMirov <hfib123@yandex.ru>
2018-07-25 22:00:33 +0500

    eval.joes

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,24 +1,24 @@
-  exports.info = {description: 'Выполнить', required: 'BOT_OWNERS'}
+exports.info = {description: 'Выполнить код', required: 'BOT_OWNERS'}
 exports.run = async (client, msg, args) => {
-var code = args.join(' ');
-    try {
-      let evaled = eval(code);
-      if (!code) {
-        return msg.channel.send('Для выполнения команды eval необходим код');
-      }
-      if (typeof evaled !== 'string')
-        evaled = require('util').inspect(evaled)
-        var embed = new Discord.RichEmbed()
-          .setTitle(`Эвал успешно выполнен`)
-          .setColor('0x4f351')
-          .setDescription(`📥 Input: \n \`\`\`${code}\`\`\` \n 📤 Output: \n  \`\`\`${(evaled)}\`\`\``)
-        msg.channel.send({embed});
-    } catch (err) {
-      var embed = new Discord.RichEmbed()
-        .setTitle('Ошибка выполнения кода')
-        .setColor('0xff0202')
-        .setDescription(`📥 Input: \n \`\`\`${code}\`\`\`\n 📤 Output:\n  \`\`\`${(err)}\`\`\``)
+  var code = args.join(' ');
+  try {
+    let evaled = eval(code);
+    if (!code) {
+      return msg.channel.send('Для выполнения команды eval необходим код');
+    }
+    if (typeof evaled !== 'string')
+      evaled = require('util').inspect(evaled)
+      var embed = new client.RichEmbed()
+        .setTitle(`Эвал успешно выполнен`)
+        .setColor('4F351')
+        .setDescription(`📥 Input: \n \`\`\`${code}\`\`\` \n 📤 Output: \n  \`\`\`${(evaled)}\`\`\``)
       msg.channel.send({embed});
-      msg.delete();
-   }
- }
+   } catch (err) {
+     var embed = new client.RichEmbed()
+       .setTitle('Ошибка выполнения кода')
+       .setColor('FF0202')
+       .setDescription(`📥 Input: \n \`\`\`${code}\`\`\`\n 📤 Output:\n  \`\`\`${(err)}\`\`\``)
+     msg.channel.send({embed});
+     msg.delete();
+  }
+}
