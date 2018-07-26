bfbfd070111bf628b2c963b3d4a93a650baf1e29
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-26 14:59:46 +0300

    Create userinfo.js

---
+++ cmds/userinfo.js
@@ -0,0 +1,6 @@
+  var u1 = msg.mentions.users.first()
+  var embed = new client.RichEmbed()
+  .setTitle("Информация о " + '<@${u1.tag}>')
+  .setDescription('Никнейм\n${u1.nickname}\n\nАйди\n${u1.id}\n\nДискриминатор\n${u1.discriminator}')
+  .setFooter("AMS")
+  msg.channel.send({embed})
