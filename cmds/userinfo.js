d784dfe15cee7033aeb112a3341cc6ef22abc899
DanilMirov <hfib123@yandex.ru>
2018-07-26 19:05:00 +0500

    .

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,18 +1,14 @@
 exports.info = {description: 'Информация о пользователе', required: 'SEND_MESSAGES'}
 exports.run = async (client, msg, args) => {
-  var u1 = msg.mentions.users.first()
+  var u = msg.mentions.users.first() || Object.assign(msg.author, msg.member)
   var embed = new client.RichEmbed()
-  .setTitle("Информация о " + '<@${u1.tag}>')
-  .addField("Ник")
-  .addValue('${u1.nickname}')
-  .addField("Айди")
-  .addValue('${u1.id}')
-  .addField("Дискриминатор")
-  .addValue('${u1.discriminator}')
-  .addField("Айди последнего сообщения")
-  .addValue('${u1.lastMessgeID')
-  .setFooter("AMS")
+    .setTitle(`Информация о <@${u.tag}>`)
+    .addField('Ник', u.nickname || u.username)
+    .addField('Айди', u.id)
+    .addField('Дискриминатор', u.discriminator)
+    .addField('Айди последнего сообщения', u.lastMessageID)
+    .addField('Тип аккаунта', u.bot ? 'Бот' : 'Юзер')
+    .setImage(u.avatarURL)
+    .setFooter('AMS')
   msg.channel.send({embed})
-
-  if(!u1) return msg.reply("Вы не указали пользователя.")
-  }
+}
--- cmds/vote.js
+++ cmds/vote.js
@@ -1,15 +1,14 @@
 exports.info = {description: 'Голосование', required: 'SEND_MESSAGES'}
 exports.run = async (client, msg, args) => {
-    var embed = new client.RichEmbed()
-    .setTitle("Голосование")
-    .setDescription(args.join(" "))
+  var embed = new client.RichEmbed()
+    .setTitle('Голосование')
+    .setDescription(args.join(' '))
     .setColor('RANDOM')
-    .setFooter('AMS');
-    msg.guild.channels.get('471794934354018306').send({embed}).then(async (sent) => {
-  await sent.react('✅')
-  await sent.react('❌')
-  await sent.react('✅')
+    .setFooter('AMS')
+  var m = await (msg.guild.channels.find('name', 'votes')).send({embed})
+  await m.react('✅')
+  await m.react('❌')
+  await m.react('✅')
   return msg.reply('**Голосование успешно начато**')
-        })
 }
  
