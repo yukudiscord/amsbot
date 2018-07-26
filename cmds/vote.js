4ba121589297eb7a63cca351d8458e34f7508fba
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-26 11:47:37 +0300

    Update vote.js

--- cmds/vote.js
+++ cmds/vote.js
@@ -1,13 +1,14 @@
  exports.info = {description: 'Голосование', required: 'BOT_OWNERS'}
 exports.run = async (client, msg, args) => {
-
 var vote = args.join(" ");
 var embed = new Discord.RichEmbed()
 .setTitle("Голосование")
 .setDescription(vote)
 .setColor('RANDOM')
-.setFooter('Poll');
-msg.guild.channels.find('name', 'votes').send({embed})
-await msg.react('${client.emojis.get(client.yes)}')
+.setFooter('AMS');
+client.fetchwebhook('471794934354018306, 'process.env.WEBHOOK_ID').then(webhook -> {
+      webhook.send('', {username: nick, avatarURL: msg.author.avatarURL, embeds:  [embed]}).catch(console.error);
+await msg.react(client.emojis.get(client.yes))
 await msg.react(client.emojis.get(client.no))
+ }
 }
