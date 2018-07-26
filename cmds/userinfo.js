73697ff70d55cc6dfd7cf354653e1f4de248fee6
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-26 15:02:04 +0300

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -4,3 +4,5 @@
   .setDescription('Никнейм\n${u1.nickname}\n\nАйди\n${u1.id}\n\nДискриминатор\n${u1.discriminator}')
   .setFooter("AMS")
   msg.channel.send({embed})
+
+  if(!u1) return msg.reply("Вы не указали пользователя.")
