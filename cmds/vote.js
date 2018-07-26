d8811921be5e7fe301c4765894651986652389fe
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-26 11:53:54 +0300

    Update vote.js

--- cmds/vote.js
+++ cmds/vote.js
@@ -1,4 +1,4 @@
- exports.info = {description: 'Голосование', required: 'SEND_MESSAGES'}
+exports.info = {description: 'Голосование', required: 'SEND_MESSAGES'}
 exports.run = async (client, msg, args) => {
  
 var vote = args.join(" ");
@@ -9,7 +9,7 @@ var embed = new Discord.RichEmbed()
 .setFooter('AMS');
 client.fetchwebhook('471794934354018306, 'process.env.WEBHOOK_ID').then(webhook -> {
       webhook.send('', {username: nick, avatarURL: msg.author.avatarURL, embeds:  [embed]}).catch(console.error);
-await msg.react(client.emojis.get(client.yes))
-await msg.react(client.emojis.get(client.no))
+await msg.react('${client.emojis.get(client.yes)}')
+await msg.react('${client.emojis.get(client.no)}')
  }
 }
