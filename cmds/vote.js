3e2610a70a5ce27a896e5d8d7c7e68e7dafe466d
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-26 11:42:24 +0300

    Create vote.js

---
+++ cmds/vote.js
@@ -0,0 +1,13 @@
+ exports.info = {description: 'Голосование', required: 'BOT_OWNERS'}
+exports.run = async (client, msg, args) => {
+
+var vote = args.join(" ");
+var embed = new Discord.RichEmbed()
+.setTitle("Голосование")
+.setDescription(vote)
+.setColor('RANDOM')
+.setFooter('Poll');
+msg.guild.channels.find('name', 'votes').send({embed})
+await msg.react('${client.emojis.get(client.yes)}')
+await msg.react(client.emojis.get(client.no))
+}
