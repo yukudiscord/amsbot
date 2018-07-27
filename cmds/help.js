d99d7788df0c8d9949394ed38a9d57fef73d9a14
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-07-27 15:26:53 +0300

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -32,6 +32,6 @@ exports.run = async (client, msg, args) => {
       .setTitle('Помощь')
       .setDescription(res)
       .setColor('FFFFFF')
-    msg.author.send({embed}
+    msg.author.send({embed})
   }
 }
